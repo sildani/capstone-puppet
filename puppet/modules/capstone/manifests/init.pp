45c61bcc39884a9c41e3b59f18fcb980ee163ab0
Daniel Silva <daniel@silvanolte.com>
2013-08-23 17:28:59 -0700

    Added the dependent modules, and started capstone with a simple apache install just to see it work.

---
+++ puppet/modules/capstone/manifests/init.pp
@@ -0,0 +1,5 @@
+class capstone {
+  
+  class { 'apache' }
+
+}
--- vagrant/manifests/init.pp
+++ vagrant/manifests/init.pp
@@ -3,3 +3,12 @@ File { owner => 0, group => 0, mode => 0644 }
 file { '/etc/motd':
   content => "Welcome to your Vagrant-built virtual machine! Managed by Puppet.\n"
 }
+
+file { '/opt/capstone':
+  ensure => 'directory'
+}
+
+file { '/opt/capstone/puppet':
+   ensure => 'link',
+   target => '/vagrant/puppet',
+}
